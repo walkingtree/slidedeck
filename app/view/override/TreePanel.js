a2b16fc76ee39814e693063765ecfbeb0b78db22
ranjit-battewad <ranjit.n.battewad@walkingtree.in>
2015-04-28 16:14:15 +0530

    Pushing changes for search item highlight

--- app/view/override/TreePanel.js
+++ app/view/override/TreePanel.js
@@ -1,7 +1,10 @@
 Ext.define('SD.view.override.TreePanel', {
     override: 'SD.view.TreePanel',
     plugins: [
-                Ext.create('wtc.ux.plugin.TreeSearch', {})
+                Ext.create('wtc.ux.plugin.TreeSearch', {
+                    highlightSearch:true,
+                    highlightCls:'searchitem-highlight-cls'
+                })
             ]
     
 });
--- wtc/ux/plugin/TreeSearch.js
+++ wtc/ux/plugin/TreeSearch.js
@@ -53,150 +53,161 @@
  */
 
 Ext.define('wtc.ux.plugin.TreeSearch', {
-  alias:'plugin.treesearch',
+    alias:'plugin.treesearch',
 
-  extend:'Ext.AbstractPlugin',
-  
-  config: {
-      
-      /**
+    extend:'Ext.AbstractPlugin',
+
+    config: {
+
+        /**
         * @cfg {Boolean} collapseOnClear
         * True to collpase tree after clearsearch.
         */
-       collapseOnClear: true,
+        collapseOnClear: true,
 
-       /**
+        /**
         * @cfg {Boolean} allowParentFolders
         * True to allow parents folder in tree search.
         */
-       allowParentFolders: true,
+        allowParentFolders: true,
 
-       /**
+        /**
         * @cfg {String} collapseAllTooltip
         * Tooltip text for collapseAll button.
         */
-       collapseAllTooltip: 'Collapse All',
+        collapseAllTooltip: 'Collapse All',
 
-       /**
+        /**
         * @cfg {String} expandAllTooltip
         * Tooltip text for expandAll button.
         */
-       expandAllTooltip: 'Expand All',
+        expandAllTooltip: 'Expand All',
 
-       /**
+        /**
         * @cfg {String} collapseAllIconCls
         * cls for collapse all icon.
         */
-       collapseAllIconCls: 'collapseall',
+        collapseAllIconCls: 'collapseall',
 
-       /**
+        /**
         * @cfg {String} expandAllIconCls
         * cls for expand all icon.
         */
-       expandAllIconCls: 'expandall', 
-       
-       /**
+        expandAllIconCls: 'expandall', 
+
+        /**
         * @cfg {object} finlterFn
         * function to filter tree
         * Default to ''
         */
         filterFn: ''
-  },
+    },
 
-  /*Initialization of plugin*/
-  init:function (treeView) {
-    var me = this;
-    treeView.on('render', me.onFieldRender, me, {single: true});    
-    treeView.on('afteritemexpand', me.expandnode, '', treeView, 2);
-  },
+    /*Initialization of plugin*/
+    init:function (treeView) {
+        var me = this;
+        
+        me.highlightCls = me.config.highlightCls;
+        if(this.config.highlightSearch && Ext.isEmpty(me.config.highlightCls)){
+         
+            getHighlightCls='default-searchitem-highlight-cls';
+        }  
 
- /**
+        treeView.on('render', me.onFieldRender, me, {single: true});    
+        treeView.on('afteritemexpand', me.expandnode, '', treeView, 2);
+    },
+
+    /**
   * Function to render search component into tree panel
   * @param {object} treepanel
   */
-  onFieldRender:function (treeView) {
-     var me = this;
-     treeView.hideCollapseTool = true;
-     var toolBar = new Ext.Toolbar({
-        position: 'top'
-       ,items:[
-            {
-              xtype: 'textfield'
-             ,name: 'searchfield'
-             ,cls: 'searchFiledRadious'
-             ,enableKeyEvents: true
-             ,listeners: {
-                 keyup: function(ths, e, eOpts) {
-                     if (ths.value) {
-                         me.textentered(me, treeView, ths.value);
-                     } else {
-                         me.clearfilter(me, treeView);
-                     }
-                 }
-             }
-            }, {
-                    
+    onFieldRender:function (treeView) {
+        var me = this;
+
+
+
+
+        treeView.hideCollapseTool = true;
+        var toolBar = new Ext.Toolbar({
+            position: 'top'
+            ,items:[
+                {
+                    xtype: 'textfield'
+                    ,name: 'searchfield'
+                    ,cls: 'searchFiledRadious'
+                    ,enableKeyEvents: true
+                    ,listeners: {
+                        keyup: function(ths, e, eOpts) {
+                            if (ths.value) {
+                                me.textentered(me, treeView, ths.value);
+                            } else {
+                                me.clearfilter(me, treeView);
+                            }
+                        }
+                    }
+                }, {
+
                     iconCls : 'collapseall',
                     width  : 23,
                     tooltip: 'Collapse All',
                     handler:function() {
-                         this.up('treepanel').collapseAll();
+                        this.up('treepanel').collapseAll();
                     }
-            }, {
-                    
-                     iconCls : 'expandall',
+                }, {
+
+                    iconCls : 'expandall',
                     tooltip: 'Expand All',
                     width  : 23,
                     handler:function() {
-                         if (this.up('treepanel').dockedItems.getAt(2).items.getAt(0).value) {
-                             me.textentered(me, treeView, this.up('treepanel').dockedItems.getAt(2).items.getAt(0).value);
-                         } else {
-                             this.up('treepanel').expandAll();
-                         }
-                         
+                        if (this.up('treepanel').dockedItems.getAt(2).items.getAt(0).value) {
+                            me.textentered(me, treeView, this.up('treepanel').dockedItems.getAt(2).items.getAt(0).value);
+                        } else {
+                            this.up('treepanel').expandAll();
+                        }
+
                     }
-            }, {
-                xtype: 'cycle'
-               ,showText: true
-               ,prependText: 'For '
-               ,menu: {
-                    id: 'view-type-menu',
-                    items: [{
-                        text: 'All Nodes'
-                       
-                       ,checked: true
-                    }, {
-                        text: 'Leaf Nodes'
-                      
-                    }]
+                }, {
+                    xtype: 'cycle'
+                    ,showText: true
+                    ,prependText: 'For '
+                    ,menu: {
+                        id: 'view-type-menu',
+                        items: [{
+                            text: 'All Nodes'
+
+                            ,checked: true
+                        }, {
+                            text: 'Leaf Nodes'
+
+                        }]
+                    }
+                    ,changeHandler: function(cycleBtn, activeItem) {                    
+                        if (activeItem.itemIndex === 1) {
+                            me.allowParentFolders = false;
+                            me.textentered(me, this.up('treepanel'), this.up('treepanel').dockedItems.getAt(2).items.getAt(0).value);
+                        } else {
+                            me.allowParentFolders = true;
+                            me.textentered(me, this.up('treepanel'), this.up('treepanel').dockedItems.getAt(2).items.getAt(0).value);
+                        }
+                    }
                 }
-               ,changeHandler: function(cycleBtn, activeItem) {                    
-                    if (activeItem.itemIndex === 1) {
-                        me.allowParentFolders = false;
-                        me.textentered(me, this.up('treepanel'), this.up('treepanel').dockedItems.getAt(2).items.getAt(0).value);
-                    } else {
-                        me.allowParentFolders = true;
-                        me.textentered(me, this.up('treepanel'), this.up('treepanel').dockedItems.getAt(2).items.getAt(0).value);
-                    }
-               }
-             }
-        ]
-    });
-    treeView.addDocked(toolBar);
-  }
-  
-  /**
+            ]
+        });
+        treeView.addDocked(toolBar);
+    }
+
+    /**
    * @event textentered
    * Fires on text entered or change into searchfield
    * @param ths plugin component
    * @param treeView tree panel
    * @param value search field value
    */
-  ,textentered: function(ths, treeView, value) {
+    ,textentered: function(ths, treeView, value) {
 
-      if(true) {
-     
-          var tree = ths
+        if(true) {
+
+            var tree = ths
             ,matches = []
             ,root = treeView.getRootNode()
             ,property = property || 'text'
@@ -206,6 +217,7 @@ Ext.define('wtc.ux.plugin.TreeSearch', {
             ,me = ths;
 
             if (Ext.isEmpty(value)) {
+                me.clearFilter();
                 return;
             }
 
@@ -213,110 +225,116 @@ Ext.define('wtc.ux.plugin.TreeSearch', {
 
             root.cascadeBy(function(node) {
                 if (node.get(property).match(re)) {
+                    if(me.config.highlightSearch === true){
+                        node.set('cls', me.highlightCls);
+                    }
                     matches.push(node);
+                } else { 
+                    node.set('cls','');
                 }
             });
 
             if(me.allowParentFolders === false) {
                 Ext.each(matches, function(match) {
-                   if (!match.isLeaf()) {
-                       Ext.Array.remove(matches, match);
-                   }
-               });
+                    if (!match.isLeaf()) {
+                        Ext.Array.remove(matches, match);
+                    }
+                });
             }
 
             Ext.each(matches, function(item, i, arr) {
-               root.cascadeBy(function(node) {
-                   if (node.contains(item) === true) {
-                       visibleNodes.push(node);
-                   }
-               });
-               if (me.allowParentFolders === true && !item.isLeaf()) {
-                   item.cascadeBy(function(node) {
-                       visibleNodes.push(node);
-                   });
-               }
-               visibleNodes.push(item);
-           });
+                root.cascadeBy(function(node) {
+                    if (node.contains(item) === true) {
+                        visibleNodes.push(node);
+                    }
+                });
+                if (me.allowParentFolders === true && !item.isLeaf()) {
+                    item.cascadeBy(function(node) {
+                        visibleNodes.push(node);
+                    });
+                }
+                visibleNodes.push(item);
+            });
 
-           root.cascadeBy(function(node) {
-               viewNode = Ext.fly(treeView.getView().getNode(node));
-               if (viewNode) {
-                   viewNode.setVisibilityMode(Ext.Element.DISPLAY);
-                   viewNode.setVisible(Ext.Array.contains(visibleNodes, node));
-               }
-           });
-      } else {
-   
-         
-      }
-  }
-  
-  /**
+            root.cascadeBy(function(node) {
+                viewNode = Ext.fly(treeView.getView().getNode(node));
+                if (viewNode) {
+                    viewNode.setVisibilityMode(Ext.Element.DISPLAY);
+                    viewNode.setVisible(Ext.Array.contains(visibleNodes, node));
+                }
+            });
+        } else {
+
+
+        }
+    }
+
+    /**
    * @event clearfilter
    * Fires on searchfield value is null
    * @param ths plugin component
    * @param treeView tree panel
    */
-  ,clearfilter: function(ths, treeView) {
-      var me = ths
-     ,root = treeView.getRootNode();
+    ,clearfilter: function(ths, treeView) {
+        var me = ths
+        ,root = treeView.getRootNode();
 
-      if (me.collapseOnClear) {
-         treeView.collapseAll();
-      }
-      root.cascadeBy(function(node) {
-         viewNode = Ext.fly(treeView.getView().getNode(node));
-         if (viewNode) {
-             viewNode.show();
-         }
-      });
-  }
-  
-  /**
+        if (me.collapseOnClear) {
+            treeView.expandAll();
+        }
+        root.cascadeBy(function(node) {
+            node.set('cls','');
+            viewNode = Ext.fly(treeView.getView().getNode(node));
+            if (viewNode) {
+                viewNode.show();
+            }
+        });
+    }
+
+    /**
    * @event expandnode
    * Fires on expanding tree panel node manually by clicking on node
    * @param node expended node of tree
    * @param index index of node
    * @param {object} treeView description
    */
-  ,expandnode: function(node, index, root, treeView, itemPostion) {
-      if(!itemPostion) {
-          itemPostion = 2;
-      }
-      var value = treeView.dockedItems.getAt(itemPostion).items.getAt(0).value;
-          
+    ,expandnode: function(node, index, root, treeView, itemPostion) {
+        if(!itemPostion) {
+            itemPostion = 2;
+        }
+        var value = treeView.dockedItems.getAt(itemPostion).items.getAt(0).value;
 
-      var matches = []
-          ,root = node
-          ,property = property || 'text'
-          ,re = re || new RegExp(value, "ig")
-          ,visibleNodes = [];
 
-      if(value) {
-        root.cascadeBy(function(node) {
-              if (node.get(property).match(re)) {
-                  matches.push(node);
-              }
-        });
+        var matches = []
+        ,root = node
+        ,property = property || 'text'
+        ,re = re || new RegExp(value, "ig")
+        ,visibleNodes = [];
 
-        Ext.each(matches, function(item, i, arr) {
-              root.cascadeBy(function(node) {
-                  if (node.contains(item) === true) {
-                      visibleNodes.push(node);
-                  }
-              });
-              visibleNodes.push(item);
-         });
+        if(value) {
+            root.cascadeBy(function(node) {
+                if (node.get(property).match(re)) {
+                    matches.push(node);
+                }
+            });
 
-         root.cascadeBy(function(node) {
-              viewNode = Ext.fly(treeView.getView().getNode(node));
-              if (viewNode) {
-                  viewNode.setVisibilityMode(Ext.Element.DISPLAY);
-                  viewNode.setVisible(Ext.Array.contains(visibleNodes, node));
-              }
-         });
-       }
-       //myMask.hide();
-  }
+            Ext.each(matches, function(item, i, arr) {
+                root.cascadeBy(function(node) {
+                    if (node.contains(item) === true) {
+                        visibleNodes.push(node);
+                    }
+                });
+                visibleNodes.push(item);
+            });
+
+            root.cascadeBy(function(node) {
+                viewNode = Ext.fly(treeView.getView().getNode(node));
+                if (viewNode) {
+                    viewNode.setVisibilityMode(Ext.Element.DISPLAY);
+                    viewNode.setVisible(Ext.Array.contains(visibleNodes, node));
+                }
+            });
+        }
+        //myMask.hide();
+    }
 });
